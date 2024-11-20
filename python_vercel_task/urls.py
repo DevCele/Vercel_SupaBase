025db22188c0440a6493b3dcd7645e306b7616d4
CeleJS <celestejimenez102@gmail.com>
2024-11-20 11:41:27 -0300

    actualizacion

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Blog App</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', home),  # Ruta para la raíz
 ]
