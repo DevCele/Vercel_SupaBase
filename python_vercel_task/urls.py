72ef0e86bf052d4c44e6afb283f9c721bc4b4065
CeleJS <celestejimenez102@gmail.com>
2024-11-20 21:41:38 -0300

    change url.py

--- python_vercel_task/urls.py
+++ python_vercel_task/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home(request):
     return HttpResponse("<h1>Welcome to Blog App</h1>")
@@ -9,3 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
